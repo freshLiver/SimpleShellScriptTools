48c631f786155dfa640d329b09ef95be048d15c0
freshLiver <freshliver.cys@gmail.com>
2020-12-30 18:45:23 +0800

    show GiB (perc)

--- MemUsage.sh
+++ MemUsage.sh
@@ -1,1 +1,9 @@
-free -m | sed "s/ +/ /g" | awk 'NR==2 {printf "RAM: %2.0f%\n", 100-($7/$2*100)}'
+MemRaw=$(free -m | sed "s/ +/ /g" | awk 'NR==2')
+
+Used_MiB=$(echo $MemRaw | awk '{print $2-$7}')
+Used_perc=$(echo $MemRaw | awk '{print 1-($7/$2)}')
+
+# echo $Used_MiB 
+# echo $Used_perc
+
+echo $(echo "$Used_MiB $Used_perc" | awk '{printf "%3.1f GiB (%3.1f%)" , $1/1024, $2*100}')
