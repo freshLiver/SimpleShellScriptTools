79303ecaeccca3a67d021cf62f6f35dee993a5c7
freshLiver <freshliver.cys@gmail.com>
2020-12-30 18:57:09 +0800

    easy to set on/off symbol

--- KeyboardLockState.sh
+++ KeyboardLockState.sh
@@ -1,20 +1,26 @@
 stat=$(xset q | grep "00: C" | sed -r "s/ {2,}/ /g" | awk '{print $4, $8}')
 
+Caps_off="Ⓐ"
+Caps_on="🅐"
+
+Num_off="①"
+Num_on="❶"
+
 if [ "$stat" = "off off" ]
 then
-    echo "|   |   |"
+    echo "| $Caps_off | $Num_off |"
 
 elif [ "$stat" = "off on" ]
 then
-    echo "|   | 1 |"
+    echo "| $Caps_off | $Num_on |"
 
 elif [ "$stat" = "on off" ]
 then
-    echo "| A |   |"
+    echo "| $Caps_on | $Num_off |"
 
 elif [ "$stat" = "on on" ]
 then
-    echo "| A | 1 |"
+    echo "| $Caps_on | $Num_on |"
     
 else
     echo "XXXXXXXX"
