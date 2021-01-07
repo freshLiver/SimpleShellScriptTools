969dc830dacdab81e255797d77b6ed427cb3c102
freshLiver <freshliver.cys@gmail.com>
2021-01-07 19:39:57 +0800

    space before %

--- MemUsage.sh
+++ MemUsage.sh
@@ -6,4 +6,4 @@ Used_perc=$(echo $MemRaw | awk '{print ($8/$4)}')
 # echo $Used_MiB 
 # echo $Used_perc
 
-echo $(echo "$Used_MiB $Used_perc" | awk '{printf "%3.1f GiB (%3.1f%c)", $1/1024, $2*100, "%"}')
+echo $(echo "$Used_MiB $Used_perc" | awk '{printf "%3.1f GiB (%3.1f %c)", $1/1024, $2*100, "%"}')
