b29114638b594d4a72600971a9b267c619b09fe5
freshLiver <freshliver.cys@gmail.com>
2021-01-07 19:37:26 +0800

    use top, instead of free

--- MemUsage.sh
+++ MemUsage.sh
@@ -1,9 +1,9 @@
-MemRaw=$(free -m | sed "s/ +/ /g" | awk 'NR==2')
+MemRaw=$(top -n 1 | grep "KiB Mem")
 
-Used_MiB=$(echo $MemRaw | awk '{print $2-$7}')
-Used_perc=$(echo $MemRaw | awk '{print 1-($7/$2)}')
+Used_MiB=$(echo $MemRaw | awk '{print ($8)/1024}')
+Used_perc=$(echo $MemRaw | awk '{print ($8/$4)}')
 
 # echo $Used_MiB 
 # echo $Used_perc
 
-echo $(echo "$Used_MiB $Used_perc" | awk '{printf "%3.1f GiB (%3.1f%)" , $1/1024, $2*100}')
+echo $(echo "$Used_MiB $Used_perc" | awk '{printf "%3.1f GiB (%3.1f%c)", $1/1024, $2*100, "%"}')
