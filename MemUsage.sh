80f7317c942c6c385ed344adc4822c4425351364
freshLiver <freshliver.cys@gmail.com>
2021-01-15 01:22:37 +0800

    MemRaw get nothing in Plasma Command Output Applet (verion 11)

--- MemUsage.sh
+++ MemUsage.sh
@@ -1,9 +1,18 @@
+# !/bin/bash
 MemRaw=$(top -n 1 | grep "KiB Mem")
 
 Used_MiB=$(echo $MemRaw | awk '{print ($8)/1024}')
 Used_perc=$(echo $MemRaw | awk '{print ($8/$4)}')
 
+if [ "$MemRaw" = "" ]
+then
+    MemRaw=$(free | grep "Mem")
+    Used_MiB=$(echo $MemRaw | awk '{print $3/1024}')
+    Used_perc=$(echo $MemRaw | awk '{print $3/$2}')
+fi
+
+echo $(echo "$Used_MiB $Used_perc" | awk '{printf "%3.1f GiB (%3.1f %c)", $1/1024, $2*100, "%"}')
+
 # echo $Used_MiB 
 # echo $Used_perc
 
-echo $(echo "$Used_MiB $Used_perc" | awk '{printf "%3.1f GiB (%3.1f %c)", $1/1024, $2*100, "%"}')
